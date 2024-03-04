3534cdcc84b01e40cc5bd0d2687b364e7273951c
lucasew <lucas59356@gmail.com>
2024-03-04 12:47:45 -0300

    ah pronto que era só o go empurrando o gvisor demais e quebrando tudo
    ty @bradfitz from https://github.com/tailscale/tailscale/issues/7596#issuecomment-1474907674
    
    Signed-off-by: lucasew <lucas59356@gmail.com>

---
+++ deps.go
@@ -0,0 +1,6 @@
+package tsproxy
+
+// explicit deps so go mod tidy dont remove them from go.mod
+import (
+	_ "gvisor.dev/gvisor/pkg/refs"
+)
--- package.nix
+++ package.nix
@@ -6,5 +6,13 @@ buildGo122Module {
 
   src = ./.;
 
-  vendorHash = "sha256-DmmEXXXi+19+OcW6DTQ2bCbebIldDdkMRjvm6Dp3Df0=";
+  # vendorHash = "sha256:${lib.fakeSha256}";
+  vendorHash = "sha256-N8/n/FTZf59bQ1FZ1jf1RaUwsjmg8pwYKkJPleoy8fU=";
+
+  postConfigure = ''
+    # chmod -R +w vendor/gvisor.dev/gvisor #/pkg/refs/refs_template.go
+    # rm vendor/gvisor.dev/gvisor/pkg/refs/refs_template.go
+    # substituteInPlace vendor/gvisor.dev/gvisor/pkg/refs/refs_template.go \
+    #   --replace refs_template refs
+  '';
 }
