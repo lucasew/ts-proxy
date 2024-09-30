f11d95e3194a5b13667251115a0e679e4d0c66cb
A helpful machine <actions@github.com>
2024-09-30 00:47:14 +0000

    Update package dependencies + tidy
    Automatic update to the project's package dependencies initiated by an
    automatic GitHub Action running on cron. Keeps upgrades less of a
    monolithic task and lets security-related patches trickle in more
    quickly.

--- package.nix
+++ package.nix
@@ -10,7 +10,7 @@ buildGo123Module {
   CGO_ENABLED = 0;
 
   # vendorHash = "sha256:${lib.fakeSha256}";
-  vendorHash = "sha256-R24pojGSerIg9va23VEUCwlXLiT/E+CE5X5B+5E9uWg=";
+  vendorHash = "sha256-8fyyGXSzM5B0u71o+Np8/JvfP0dR9O48cNMt0YsnPR4=";
 
   postConfigure = ''
     # chmod -R +w vendor/gvisor.dev/gvisor #/pkg/refs/refs_template.go
