970fb39b05a66f6e4e4959b41f1ea75aa0633c00
lucasew <lucas59356@gmail.com>
2024-10-13 16:17:14 -0300

    expose the http port in port 80 instead of 443 if not using funnel
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- cmd/ts-proxyd/main.go
+++ cmd/ts-proxyd/main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/lucasew/ts-proxy"
 	"log"
 	"net/url"
+	"github.com/davecgh/go-spew/spew"
 )
 
 var options tsproxy.TailscaleProxyServerOptions
@@ -16,9 +17,15 @@ func init() {
 	flag.BoolVar(&options.EnableFunnel, "f", false, "Enable tailscale funnel")
 	flag.StringVar(&options.Hostname, "n", "", "Hostname in tailscale devices list")
 	flag.StringVar(&options.StateDir, "s", "", "State directory")
-	flag.StringVar(&options.Addr, "addr", ":443", "Port to listen")
+	flag.Parse()
+	defaultPort := ":80"
+	if options.EnableFunnel {
+		defaultPort = ":443"
+	}
+	flag.StringVar(&options.Addr, "addr", defaultPort, "Port to listen")
 	flag.Parse()
 	options.Upstream, err = url.Parse(remoteHost)
+	spew.Dump(options)
 	if err != nil {
 		log.Fatal(err)
 	}
